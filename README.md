f4b4fdbc257fb9e4450e6475890ceaf0012e678c
oskytar <oscar.miras@gmail.com>
2021-03-30 00:46:45 +0200

    Singulars exercises adapted

--- README.md
+++ README.md
@@ -6,5 +6,10 @@ HTML injecting into the DOM and the logic app just with vanilla JS, no framework
 ![Screenshot](./docs/capture.png)
 ####  🕹️ Demo > https://efrensanchez.github.io/Shopping-app/
 
+# Work to do
+You need to modify this project in order to achieve some new features
+
+1. Modify the app to allow the user to also set the year of the product; as shown in the demo. You'll have to modify the class Product
+2. The DELETE functionality works in a funny way. It does not matter in which area of the item you click: the item is always removed; even though you don't click the "Delete" button. How could you solve it? Why does it happen?
 
 
--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 //Product Constructor
 class Product {
-  constructor(name, price, year) {
+  constructor(name, price) {
     this.name = name;
     this.price = price;
-    this.year = year;
   }
 }
 
 //UI Constructor
 class UI {
   //Product template
-  addProduct(product) {
+  static addProduct(product) {
     const productList = document.getElementById("product-list");
     const element = document.createElement("div");
     element.innerHTML = `
@@ -18,7 +17,6 @@ class UI {
         <div class="card-body">
           <h5><strong>${product.name}</strong></h5>
           <strong >Price</strong>: ${product.price}€ 
-          <strong class="ml-4">Year</strong>: ${product.year} 
           <a href="#" class="btn btn-danger ml-5" name="delete">Delete</a>
         </div>
       </div>
@@ -26,18 +24,17 @@ class UI {
     productList.appendChild(element);
   }
 
-  resetForm() {
+  static resetForm() {
     document.getElementById("product-form").reset();
   }
 
-  deleteProduct(element) {
-    if (element.name === "delete") {
-      element.parentElement.parentElement.remove();
-      return true;
-    }
+  static deleteProduct(element) {
+    console.log("element", element)
+    element.parentElement.parentElement.remove();
+    return true;
   }
 
-  showMessage(message, cssClass) {
+  static showMessage(message, cssClass) {
     const msg = document.createElement("div");
     msg.className = `alert alert-${cssClass} mt-2 text-center`;
     msg.appendChild(document.createTextNode(message));
@@ -50,38 +47,35 @@ class UI {
     container.insertBefore(msg, app);
 
     //Remove after 2 seconds
-    setTimeout(function() {
+    setTimeout(function () {
       document.querySelector(".alert").remove();
-    }, 1000);
+    }, 2000);
   }
 }
 
 //DOM Events
 document.getElementById("product-form").addEventListener("submit", e => {
-  const name = document.getElementById("product-name").value,
-    price = document.getElementById("product-price").value,
-    year = document.getElementById("product-year").value;
+  const name = document.getElementById("product-name").value
+    price = document.getElementById("product-price").value
+
 
   //Create a new Object Product
-  const product = new Product(name, price, year);
+  const product = new Product(name, price);
 
-  //Create a new UI
-  const ui = new UI();
 
   //Save product
-  ui.addProduct(product);
-  ui.resetForm();
-  ui.showMessage("Product added successfully", "success");
+  UI.addProduct(product);
+  UI.resetForm();
+  UI.showMessage("Product added successfully", "success");
 
   e.preventDefault();
 });
 
 //Delete product
 document.getElementById("product-list").addEventListener("click", e => {
-  const ui = new UI();
-  ui.deleteProduct(e.target);
-  if(ui.deleteProduct(e.target)) {
-    ui.showMessage("Product removed successfully", "danger");
+  UI.deleteProduct(e.target);
+  if (UI.deleteProduct(e.target)) {
+    UI.showMessage("Product removed successfully", "danger");
   }
   e.preventDefault();
 });
